442521441465442712fae96f26b913f2d135cc3e
Sun Jianfei <113086278+effson@users.noreply.github.com>
2026-04-27 23:11:03 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import uuid
+
+from fastapi import FastAPI, Request
+from app.api.routers.query_router import query_router
+from app.core.lifespan import lifespan
+from app.core.context import request_id_ctx_var
+app = FastAPI(lifespan=lifespan)
+app.include_router(query_router)
+
+# 添加中间件，在每个请求中生成唯一的request_id
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    # 调用路径函数之前
+    request_id_ctx_var.set(uuid.uuid4())
+    # 调用路径函数
+    response = await call_next(request)
+    # 调用路径函数之后
+    return response
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
