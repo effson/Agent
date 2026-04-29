a0dceddc5b659b9777c141bf98a7aee976f9703b
Sun Jianfei <113086278+effson@users.noreply.github.com>
2026-04-29 12:44:50 +0800

    Update validate_sql.py

--- app/agent/nodes/validate_sql.py
+++ app/agent/nodes/validate_sql.py
@@ -1,10 +1,14 @@
 from langgraph.runtime import Runtime
+from langsmith import traceable
 
 from app.agent.context import DataAgentContext
 from app.agent.state import DataAgentState
 from app.repositories.mysql.dw.dw_mysql_repository import DWMySQLRepository
 from app.core.log import logger
+from dotenv import load_dotenv
+load_dotenv()
 
+@traceable
 async def validate_sql(state: DataAgentState, runtime: Runtime[DataAgentContext]):
     writer = runtime.stream_writer
     writer({"type": "progress", "step": "验证SQL", "status": "running"})
